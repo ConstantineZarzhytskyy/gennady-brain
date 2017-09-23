09657b4c139da1874957a22dd23b14f21b364dd5
Constantine Zarzhytskyy <951953qaz>
2017-09-24 02:22:37 +0300

    Init BrainJS

--- app/seneca-init.js
+++ app/seneca-init.js
@@ -2,12 +2,13 @@
 
 var env = process.env.NODE_ENV || 'development';
 var options = require('../options/' + env  + '.js');
-var seneca = require('seneca')();
+var seneca = require('seneca')(options.main);
 
 seneca
     .use('entity')
     .use('mongo-store', options['mongo-store'])
-    .use('../lib/brain');
+    .use('../lib/gennady')
+    .use('../lib/db');
 
 seneca.ready(function() {
   console.log('Seneca connect to database [' + options['mongo-store'].name + ']');
